05b8aa9681ad2cc657dbb54d9a1ce4b703cb476d
cxy <37500268+chenxygh@users.noreply.github.com>
2018-12-25 15:29:43 +0800

    step: 4  输入有误，窗口抖动; 输入邮箱，动态显示头像, ajax 请求

---
+++ admin/api/avatar.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ *
+ * 根据 email ，获取用户头像
+ * 通过 ? 参数获取 email
+ * email 没有，或者有误等等
+ * 返回 error
+ * 
+ */
+
+require_once '../../config.php';
+
+header('Content-Type: application/json');
+
+// 获取邮箱
+if (empty($_GET['email'])) {
+	echo '{"error": "fuck"}';
+	exit();
+}
+
+$email = $_GET['email'];
+
+// 连接数据库
+$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+if (!$conn) {
+	echo '{"error": "fuck"}';
+	exit();
+}
+
+$select = "select avatar from users where email = '{$email}' limit 1;";
+$query = mysqli_query($conn, $select);
+if (!$query) {
+	echo '{"error": "fuck"}';
+	exit();
+}
+$avatar = mysqli_fetch_assoc($query);
+
+if (!$avatar) {
+	echo '{"error": "fuck"}';
+	exit();
+}
+
+echo json_encode($avatar);
